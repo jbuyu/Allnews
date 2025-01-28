9bb0d0fa437b1145b5e7dace889a4eb956ae81d9
Justus Buyu <jbuyu.code@gmail.com>
2025-01-28 06:10:21 +0300

    feat(Http error handling):sets up hono error handling

--- server/adapter.ts
+++ server/adapter.ts
@@ -10,15 +10,9 @@ const EnvSchema = z.object({
 
 const processEnv = EnvSchema.parse(process.env);
 
-console.log('process.env', process.env.DATABASE_URL)
-
-
 const queryClient = postgres(processEnv.DATABASE_URL);
 
-// console.log('queryClient', queryClient)
-
 const db = drizzle(queryClient);
-// console.log('db', db)
 const result = await db.execute("select");
 
 console.log('result', result)
--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,44 @@
+import type { ErrorResponse } from "@/shared/types";
 import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
 
 const app = new Hono();
 
 app.get("/", (c) => {
+  // throw new HTTPException(400, { message: "Bad Request" });
+  // throw new Error("This is an error");
   return c.text("Hello aloa!");
+});
+
+//error handling
+app.onError((err, c) => {
+  if (err instanceof HTTPException) {
+    const errResponse =
+      err.res ??
+      c.json<ErrorResponse>(
+        {
+          success: false,
+          error: err.message,
+          isFormError:
+            err.cause && typeof err.cause === "object" && "form" in err.cause
+              ? err.cause.form === true
+              : false,
+        },
+        err.status,
+      );
+    return errResponse;
+  }
+
+  return c.json<ErrorResponse>(
+    {
+      success: false,
+      error:
+        process.env.NODE_ENV === "production"
+          ? "Interal Server Error"
+          : (err.stack ?? err.message),
+    },
+    500,
+  );
 });
 
 export default app;
