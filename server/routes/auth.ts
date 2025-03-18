aa834d5507c51392fb7115280faa48e8af3bb4b0
Justus Buyu <jbuyu.code@gmail.com>
2025-03-18 15:49:22 +0300

    feat(Auth route setup):adds auth routes

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -2,8 +2,10 @@ import { db } from "@/adapter";
 import type { Context } from "@/context";
 import { userTable } from "@/db/schemas/auth";
 import { lucia } from "@/lucia";
+import { loggedIn } from "@/middleware/loggedIn";
 import { loginSchema, type SuccessResponse } from "@/shared/types";
 import { zValidator } from "@hono/zod-validator";
+import { eq } from "drizzle-orm";
 import { Hono } from "hono";
 import { HTTPException } from "hono/http-exception";
 import { generateId } from "lucia";
@@ -15,8 +17,6 @@ export const authRouter = new Hono<Context>().post("/signup", zValidator("form",
     const { username, password } = c.req.valid("form")
     const passwordHash = await Bun.password.hash(password)
     const userId = generateId(15)
-    console.log('userId', userId)
-    console.log('userId', typeof userId)
     try {
         await db.insert(userTable).values({
             id: userId,
@@ -39,4 +39,60 @@ export const authRouter = new Hono<Context>().post("/signup", zValidator("form",
         throw new HTTPException(500, { message: "Failed to create user" });
     }
 
+}).post("/login", zValidator("form", loginSchema), async (c) => {
+    const { username, password } = c.req.valid("form");
+
+    const [existingUser] = await db
+        .select()
+        .from(userTable)
+        .where(eq(userTable.username, username))
+        .limit(1);
+
+    if (!existingUser) {
+        throw new HTTPException(401, {
+            message: "Incorrect username",
+            cause: { form: true },
+        });
+    }
+
+    const validPassword = await Bun.password.verify(
+        password,
+        existingUser.password_hash,
+    );
+    if (!validPassword) {
+        throw new HTTPException(401, {
+            message: "Incorrect password",
+            cause: { form: true },
+        });
+    }
+
+    const session = await lucia.createSession(existingUser.id, { username });
+    const sessionCookie = lucia.createSessionCookie(session.id).serialize();
+
+    c.header("Set-Cookie", sessionCookie, { append: true });
+
+    return c.json<SuccessResponse>(
+        {
+            success: true,
+            message: "Logged in",
+        },
+        200,
+    );
+}).get("/logout", async (c) => {
+    const session = c.get("session");
+    if (!session) {
+        return c.redirect("/");
+    }
+
+    await lucia.invalidateSession(session.id);
+    c.header("Set-Cookie", lucia.createBlankSessionCookie().serialize());
+    return c.redirect("/");
 })
+    .get("/user", loggedIn, async (c) => {
+        const user = c.get("user")!;
+        return c.json<SuccessResponse<{ username: string }>>({
+            success: true,
+            message: "User fetched",
+            data: { username: user.username },
+        });
+    });
