f5c55a3ced36504741c315009e9064222e2386ca
Justus Buyu <jbuyu.code@gmail.com>
2025-03-20 18:25:26 +0300

    feat(Relations):Finalizes relations on upvotes

--- server/db/schemas/upvotes.ts
+++ server/db/schemas/upvotes.ts
@@ -1,4 +1,8 @@
 import { integer, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+import { postsTable } from "./posts";
+import { relations } from "drizzle-orm";
+import { userTable } from "./auth";
+import { commentsTable } from "./comments";
 
 export const postUpvotesTable = pgTable("post_upvotes", {
     id: serial("id").primaryKey(),
@@ -9,6 +13,19 @@ export const postUpvotesTable = pgTable("post_upvotes", {
         .notNull(),
 });
 
+export const postUpvoteRelations = relations(postUpvotesTable, ({ one }) => ({
+    post: one(postsTable, {
+        fields: [postUpvotesTable.postId],
+        references: [postsTable.id],
+        relationName: "postUpvotes",
+    }),
+    user: one(userTable, {
+        fields: [postUpvotesTable.userId],
+        references: [userTable.id],
+        relationName: "user",
+    }),
+}));
+
 export const commentUpvotesTable = pgTable("comment_upvotes", {
     id: serial("id").primaryKey(),
     commentId: integer("comment_id").notNull(),
@@ -17,3 +34,19 @@ export const commentUpvotesTable = pgTable("comment_upvotes", {
         .defaultNow()
         .notNull(),
 });
+
+export const commentUpvoteRelations = relations(
+    commentUpvotesTable,
+    ({ one }) => ({
+        post: one(commentsTable, {
+            fields: [commentUpvotesTable.commentId],
+            references: [commentsTable.id],
+            relationName: "commentUpvotes",
+        }),
+        user: one(userTable, {
+            fields: [commentUpvotesTable.userId],
+            references: [userTable.id],
+            relationName: "user",
+        }),
+    }),
+);
